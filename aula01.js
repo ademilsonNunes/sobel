85bf7d27b0530d2bbbcd0cf0b5d6624569ea77fc
ademilsonNunes <ti@sobelsuprema.com.br>
2019-11-21 17:46:59 +0000

    Aula 01

---
+++ aula01.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    nome: "Danilo",
+    altura: 1.75,
+    peso: 67,
+    sexo: "indefinido"
+}
+
+console.log(pessoa.nome)
+
+var nome = "Danilo"
+var idade = 24
+
+console.log( nome + idade )
+
